ee960788d3c890482eac47bb85e6b445b1c0916a
Raffaelnss <santos.raffael@escola.pr.gov.br>
2026-04-10 01:03:08 +0000

    aaa

--- sus.js
+++ sus.js
@@ -1,11 +1,29 @@
-let things = ["w", "b", "q", "6"];
+// parte2 : TIPO DOS DADOS
 
-function do_thing(array)
-{
-    for(i=0; i<things.length; i++)
-    {
-        print(things[i] == "w" ? "aaaa" : things[i])
-    }
-}
+const nomealuno = "Carlos Silva" // Tipo = string
+const nota = 8.5 // Tipo = float
+const frequencia = 80 // Tipo = int
+const estaAprovado = true; // Tipo = bool
+const RA = 12345; // Tipo = int
 
-do_thing(things);
+// 1. Definição dos dados
+const nomeAluno = "Carlos"; // string
+const nota1 = 8.0; // float
+const nota2 = 6.0; // float
+const presenca = 80; // float
+
+// 2. Cálculo da Média (Operadores Aritméticos)
+// Dica: Use parênteses para garantir a ordem de precedência!
+let media = (nota1 + nota2) / 2;
+
+// 3. Lógica de Aprovação (Operadores de Comparação e Lógicos)
+// O aluno precisa de media >= 7 E presenca >= 75
+let aprovado = (media >= 7) && (presenca >= 75);
+
+// 4. Exibição do Resultado
+console.log("Aluno: " + nomeAluno);
+console.log("Média Final: " + media);
+console.log("Status de Aprovação: " + aprovado);
+
+let precisaDeRecuperacao = !aprovado;
+console.log("Precisa de recuperação: " + precisaDeRecuperacao);
